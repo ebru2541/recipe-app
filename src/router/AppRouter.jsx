7f0ab91e8d5c957bdab6d717fdbd0037667fba08
Rose <ebrue7949@gmail.com>
2023-03-13 10:55:56 +0300

    add

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Navbar from "../components/nav/Navbar";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
+import About from "../pages/about/About";
+import Detail from "../pages/detail/Detail";
 import { GlobalStyle } from "../style/GlobalStyle";
-
+import PrivateRouter from "./PrivateRouter";
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <GlobalStyle />
-      {/* <Navbar /> */}
+    <GlobalStyle/>
       <Routes>
-       
-        <Route path="/login" element={<Login />} />
-        <Route>
-          {/* <Route path="/about" element={<About />} />
-          <Route path="/navbar" element={<Navbar />} /> */}
-          <Route path="/home" element={<Home />} />
+        <Route
+          path="/login"
+          element={
+            <Login />
+          }
+        />
+
+        <Route path="/" element={<PrivateRouter />}>
+          {user ? (
+            <>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/detail" element={<Detail />} />
+            </>
+          ) : (
+            <Navigate to="/login" />
+          )}
         </Route>
       </Routes>
     </BrowserRouter>
--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
-
+import Navbar from "../components/nav/Navbar"
 const PrivateRouter = () => {
   const user=sessionStorage.getItem("user") || false
-  return (
-    user ? <Home/> : <Navigate to="/login"/>
-  )
+  return user ? (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  ) : (
+    <Navigate to="/login" />
+  );
 }
 
 export default PrivateRouter
